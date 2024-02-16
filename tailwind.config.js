775379fc8960ad0c2a4e1d408a34aff7f9e62bf4
Jeanne Clarisse Toledo <64212136+DefyingGravity10@users.noreply.github.com>
2024-02-16 16:45:29 +0800

    feat: make front page of game

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+	content: ['./index.html', './src/**/*.{html,js}'],
+	theme: {
+		screens: {
+			xs: '363px',
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px'
+		},
+		extend: {}
+	},
+	plugins: []
+}
