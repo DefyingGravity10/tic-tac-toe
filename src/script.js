3d32fb42796254351d1e5034a62f5996dd5ca28d
Jeanne Clarisse Toledo <64212136+DefyingGravity10@users.noreply.github.com>
2024-02-16 17:43:27 +0800

    feat: add functionality in the first page

---
+++ src/script.js
@@ -0,0 +1,40 @@
+function initializeGame() {
+	const firstPage = document.getElementById("first-page");
+	const againstComputer = document.getElementById("vs-computer");
+	const againstPlayer = document.getElementById("vs-player");
+	const opponent = document.getElementById("opponent");
+	const proceedButton = document.getElementById("proceed-button");
+	const secondPage = document.getElementById("second-page");
+
+	againstComputer.classList.add("selected-mode");
+
+	againstComputer.addEventListener("click", () => {
+		if (opponent.children.length === 1) {
+			opponent.removeChild(opponent.children[0]);
+			const spn = document.createElement("span");
+			spn.appendChild(document.createTextNode("PLAYER VS COMPUTER"));
+			opponent.appendChild(spn);
+			againstComputer.classList.toggle("selected-mode");
+			againstPlayer.classList.toggle("selected-mode");
+		}
+	});
+	againstPlayer.addEventListener("click", () => {
+		if (opponent.children.length === 1) {
+			opponent.removeChild(opponent.children[0]);
+			const spn = document.createElement("span");
+			spn.appendChild(document.createTextNode("PLAYER VS PLAYER"));
+			opponent.appendChild(spn);
+			againstComputer.classList.toggle("selected-mode");
+			againstPlayer.classList.toggle("selected-mode");
+		}
+	});
+	proceedButton.addEventListener("click", () => {
+		firstPage.classList.add("hidden");
+		secondPage.classList.remove("hidden");
+
+		const chosenMode = againstComputer.classList.contains("selected-mode") ? "Computer" : "Player";
+		console.log(chosenMode);
+	});
+}
+
+initializeGame();
