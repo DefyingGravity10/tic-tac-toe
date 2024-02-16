61d4dce29bbc38a81490882f403e85da560bf1f4
Jeanne Clarisse Toledo <64212136+DefyingGravity10@users.noreply.github.com>
2024-02-16 20:00:22 +0800

    refactor: rearrange and rename variables

--- src/script.js
+++ src/script.js
@@ -1,22 +1,32 @@
 function initializeGame() {
+	// Elements from first page
 	const firstPage = document.getElementById("first-page");
 	const againstComputer = document.getElementById("vs-computer");
 	const againstPlayer = document.getElementById("vs-player");
 	const opponent = document.getElementById("opponent");
 	const proceedButton = document.getElementById("proceed-button");
+	// Elements from second page
 	const secondPage = document.getElementById("second-page");
+	const backButton = document.getElementById("back-button");
+	const startButton = document.getElementById("start-button");
+	const xSymbol = document.getElementById("x-symbol");
+	const oSymbol = document.getElementById("o-symbol");
+
+	// Elements needed by the game proper
+	const gameBoard = document.getElementById("board");
 	let chosenMode;
+	let chosenSymbol;
 
-	againstComputer.classList.add("selected-mode");
-
+	// Event listners
+	// For first page
 	againstComputer.addEventListener("click", () => {
 		if (opponent.children.length === 1) {
 			opponent.removeChild(opponent.children[0]);
 			const spn = document.createElement("span");
 			spn.appendChild(document.createTextNode("PLAYER VS COMPUTER"));
 			opponent.appendChild(spn);
-			againstComputer.classList.toggle("selected-mode");
-			againstPlayer.classList.toggle("selected-mode");
+			againstComputer.classList.toggle("selected-item");
+			againstPlayer.classList.toggle("selected-item");
 		}
 	});
 	againstPlayer.addEventListener("click", () => {
@@ -25,43 +35,37 @@ function initializeGame() {
 			const spn = document.createElement("span");
 			spn.appendChild(document.createTextNode("PLAYER VS PLAYER"));
 			opponent.appendChild(spn);
-			againstComputer.classList.toggle("selected-mode");
-			againstPlayer.classList.toggle("selected-mode");
+			againstComputer.classList.toggle("selected-item");
+			againstPlayer.classList.toggle("selected-item");
 		}
 	});
 	proceedButton.addEventListener("click", () => {
 		firstPage.classList.add("hidden");
 		secondPage.classList.remove("hidden");
-		chosenMode = againstComputer.classList.contains("selected-mode") ? "Computer" : "Player";
+		chosenMode = againstComputer.classList.contains("selected-item") ? "Computer" : "Player";
 	});
 
-	const backButton = document.getElementById("back-button");
-	const startButton = document.getElementById("start-button");
-	const gameBoard = document.getElementById("board");
-	const xSymbol = document.getElementById("x-symbol");
-	const oSymbol = document.getElementById("o-symbol");
-	let chosenSymbol;
-
+	// For second page
 	backButton.addEventListener("click", () => {
 		firstPage.classList.remove("hidden");
 		secondPage.classList.add("hidden");
 	});
 	xSymbol.addEventListener("click", () => {
-		if (!xSymbol.classList.contains("selected-mode")) {
-			xSymbol.classList.toggle("selected-mode");
-			oSymbol.classList.toggle("selected-mode");
+		if (!xSymbol.classList.contains("selected-item")) {
+			xSymbol.classList.toggle("selected-item");
+			oSymbol.classList.toggle("selected-item");
 		}
 	});
 	oSymbol.addEventListener("click", () => {
-		if (!oSymbol.classList.contains("selected-mode")) {
-			xSymbol.classList.toggle("selected-mode");
-			oSymbol.classList.toggle("selected-mode");
+		if (!oSymbol.classList.contains("selected-item")) {
+			xSymbol.classList.toggle("selected-item");
+			oSymbol.classList.toggle("selected-item");
 		}
 	});
 	startButton.addEventListener("click", () => {
 		secondPage.classList.add("hidden");
 		gameBoard.classList.remove("hidden");
-		chosenSymbol = xSymbol.classList.contains("selected-mode") ? "X" : "O";
+		chosenSymbol = xSymbol.classList.contains("selected-item") ? "X" : "O";
 		console.log(chosenMode);
 		console.log(chosenSymbol);
 	});
